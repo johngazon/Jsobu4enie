f8c0904a44998b1de973568446a8845d5b050967
John <jokrul@me.com>
2021-11-22 01:15:47 +0300

    pervoe consolnoe app

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const Name = 5;
-alert(Name);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+b = prompt("На сколько Оцените его?",""),
+c = prompt("Один из последних просмотренных фильмов?", ""),
+d = prompt("На сколько Оцените его?","");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
